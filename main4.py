3f2f4f2c497576fd0cd462be990c067fd14f77a0
huskar435 <Godkraken@tutamail.com>
2025-09-08 15:56:17 +0300

    123

--- main4.py
+++ main4.py
@@ -9,12 +9,13 @@ text = [
     "Мурлычет,любит спать на подоконнике",#кот
     "Какает в тапки,любит рыбку на завтрак",#кот
     "Скулит, грызет косточку ",#собака
+
     
   
 ]
 lable = [1,0,0,1] # 1-собака, 0-кот
 
-text_train, text_test, y_train, y_test = train_test_split(text, lable, test_size=0.33, random_state=42)
+text_train, text_test, y_train, y_test = train_test_split(text, lable, test_size=0.33, random_state=20)
 
 print(text_train)
 print(text_test)
@@ -33,6 +34,6 @@ y_pred = pipe.predict(text_test)
 print(f"Accuracy: {accuracy_score(y_test,y_pred)}")
 
 print("New:", pipe.predict(["Мяукает и какает в тапки,а еще обожает спать на диване "])[0])
-print("New:", pipe.predict(["Лает,грызет косточку"])[0])             
+print("New:", pipe.predict(["ноет, играет с мячиком"])[0])             
 
              
